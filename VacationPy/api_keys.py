35f6697fca96679b9c65646bf7ae711bea095d41
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-02-06 23:00:52 -0500

    do not work command fig, I execute pip install gmaps, next,  jupyter nbextension enable --py gmaps

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a160fd00bbc7c7caddb54e2757ff85b2"
 
 # Google API Key
 g_key = "AIzaSyAZ1iF3iSurGOxs7nwvnXmZifSw5FuM-Ao"
